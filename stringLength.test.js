9dd6f495462766f8d854fba51e7195d030866c8a
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-07-11 14:56:49 +0200

    check if length is >0 and <11

--- stringLength.test.js
+++ stringLength.test.js
@@ -3,3 +3,8 @@ const stringLength = require('./stringLength')
 test('cat has 3 charachters', () => {
   expect(stringLength('cat')).toBe(3)
 })
+
+test('string length is >= 1 and <= 10', () => {
+  expect(stringLength('cat')).toBeGreaterThan(0);
+  expect(stringLength('cat')).toBeLessThan(11);
+})
